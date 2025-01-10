a25130a5aba4d2889eb2d85211c2f6404ec9b6fd
PSergio984 <ericgabrielmanabat@plv.edu.ph>
2025-01-10 23:58:14 +0800

    asdf

--- exe/js/test.js
+++ exe/js/test.js
@@ -1,7 +1,7 @@
 function addOne(num) {
     return num + 1;
   }
-  const arr = [1, 2, 3, 4, 5];
+  const arr = [1, 2, 3, 4, 5,7,8,9];
   const mappedArr = arr.map(addOne);
   console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
   
